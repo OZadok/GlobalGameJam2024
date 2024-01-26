0605299b76108c1d6cd0bc74c15b67181f197543
Omer Zadok <OZadok@gmail.com>
2024-01-27 01:13:14 +0200

    Player animation parameters

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -1,16 +1,44 @@
+using System;
+using Events;
+using SuperMaxim.Messaging;
 using UnityEngine;
 
 public class PlayerAnimationController : MonoBehaviour
 {
     private readonly int _happinessFloat = Animator.StringToHash("happiness");
     private readonly int _isLeftBool = Animator.StringToHash("isLeft");
-    
-    
+
+    private GameObject PlayerGameObject => transform.parent.gameObject;
     
     [SerializeField] private Animator animator;
 
-    
-    
+    private void OnEnable()
+    {
+        Messenger.Default.Subscribe<HappinessChangedEvent>(OnHappinessChanged);
+        Messenger.Default.Subscribe<PlayerChangedDirectionEvent>(OnPlayerChangeDirection);
+    }
+
+    private void OnDisable()
+    {
+        Messenger.Default.Unsubscribe<HappinessChangedEvent>(OnHappinessChanged);
+        Messenger.Default.Unsubscribe<PlayerChangedDirectionEvent>(OnPlayerChangeDirection);
+    }
+
+    private void OnPlayerChangeDirection(PlayerChangedDirectionEvent obj)
+    {
+        SetIsLeft(obj.Direction <= 0f);
+    }
+
+    private void OnHappinessChanged(HappinessChangedEvent obj)
+    {
+        if (obj.GameObject != PlayerGameObject)
+        {
+            return;
+        }
+        SetHappiness(obj.Level);
+    }
+
+
     public void SetIsLeft(bool isLeft)
     {
         animator.SetBool(_isLeftBool, isLeft);
