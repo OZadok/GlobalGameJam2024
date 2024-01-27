b6bb0d5beb3cc61686bb9e5ce3023833a85777bc
Omer Zadok <OZadok@gmail.com>
2024-01-27 14:41:35 +0200

    fall more straightly do auto change direction when stuck horizontally

--- Assets/Player/PlayerMovment.cs
+++ Assets/Player/PlayerMovment.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using Events;
 using SuperMaxim.Messaging;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerMovment : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class PlayerMovment : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody;
     private float _direction = 1;
     private bool isGrounded;
+    public LayerMask GroundCheckLayerMask;
+    public Collider2D collider;
 
     private void Awake()
     {
@@ -38,21 +42,30 @@ public class PlayerMovment : MonoBehaviour
         Messenger.Default.Publish(new PlayerChangedDirectionEvent(_direction));
     }
 
-    private void FixedUpdate()
-    {
-        GroundCheck();
-    }
-
     private void GroundCheck()
     {
-        var hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f);
-        isGrounded = hit;
+        RaycastHit2D[] raycasts = new RaycastHit2D[2];
+        // var hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f,GroundCheckLayerMask);
+        
+        var amount = collider.Cast(Vector2.down,raycasts, 1f);
+        // isGrounded = hit;
+        isGrounded = amount > 0;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        // var previousGrounded = isGrounded;
+        GroundCheck();
+        
         if (!isGrounded)
         {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x * 0.9f, _rigidbody.velocity.y);
+            // Debug.Log("0 x !!");
+            // if (previousGrounded)
+            // {
+            //     
+            //     Debug.Log("0 x !!");
+            // }
             return;
         }
 
@@ -60,8 +73,37 @@ public class PlayerMovment : MonoBehaviour
         {
             return;
         }
+
+        ChangeDirectionWhenStuck();
+        
         UpdateVelocity();
     }
+
+    private float lastStuckTime = float.MaxValue;
+    private void ChangeDirectionWhenStuck()
+    {
+        //detect stuck
+        if (!isGrounded)
+        {
+            return;
+        }
+
+        if (_rigidbody.velocity.magnitude < _speed / 4f)
+        {
+            if (lastStuckTime == float.MaxValue)
+            {
+                lastStuckTime = Time.time;
+            }
+            
+            if (Time.time - lastStuckTime > 0.15f)
+            {
+                ChangeDirection();
+
+                lastStuckTime = float.MaxValue;
+            }
+        }
+        
+}
 
     private void UpdateVelocity()
     {
